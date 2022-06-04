f2da8d9860d53452ef9bad3732d879c977a1e33b
Thiago Rezende <thiago.manoel.rezende@gmail.com>
2022-06-04 12:21:50 -0300

    build: added typescript dependencies

--- src/index.js
+++
@@ -1,1 +0,0 @@
-console.log("[AnimeList v0.1.0]")
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+interface User {
+  email: string,
+  username: string,
+  password: string
+}
+
+const user: User = {
+  email: "user@domain.com",
+  username: "user",
+  password: "secret"
+}
+
+console.log("[AnimeList v0.1.0]")
