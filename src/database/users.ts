939389d4ba5dd9ee33df2867e5fa279797818920
Thiago Rezende <thiago.manoel.rezende@gmail.com>
2022-06-23 23:20:27 +0000

    feat: add user model and sample data

---
+++ src/database/users.ts
@@ -0,0 +1,8 @@
+import { User } from '@models/user'
+
+const users: Array<User> = [
+  { email: 'thiago.manoel.rezende@gmail.com', username: 'thiago-rezende', password: 'secret' },
+  { email: 'horus.he4rt@gmail.com', username: 'horus-he4rt', password: 'super_secret' }
+]
+
+export default users
---
+++ src/models/user.ts
@@ -0,0 +1,11 @@
+export class User {
+  email: string
+  username: string
+  password: string
+
+  constructor(email: string, username: string, password: string) {
+    this.email = email
+    this.username = username
+    this.password = password
+  }
+}
