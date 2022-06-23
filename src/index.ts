22a95c32ad715b104b29567d54b19448d49dba6a
Thiago Rezende <thiago.manoel.rezende@gmail.com>
2022-06-23 22:16:53 +0000

    build: add ts-node-dev and express

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
-interface User {
-  email: string,
-  username: string,
-  password: string
-}
+import express, { Request, Response } from 'express'
 
-const user: User = {
-  email: 'user@domain.com',
-  username: 'user',
-  password: 'secret'
-}
+const app = express()
 
-console.log('[AnimeList v0.1.0]')
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-console.table(user)
+app.get('/', (_req: Request, res: Response) => {
+  res.status(200).json({ message: 'Anime List', version: '0.1.0' })
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log(`[anime-list]: listening on port ${port}`))
