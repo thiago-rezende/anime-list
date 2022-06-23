9c3f415ecea974bae365b6ca546c1d231da1ecfe
Thiago Rezende <thiago.manoel.rezende@gmail.com>
2022-06-23 23:57:39 +0000

    feat: add users view

--- src/routes/v1/users.ts
+++ src/routes/v1/users.ts
@@ -1,13 +1,12 @@
 import { Router, Request, Response } from 'express'
 
 import database from '@database/index'
+import { usersView } from '@views/users'
 
 const users = Router()
 
 users.get('/', (_req: Request, res: Response) => {
-  res.status(200).json({
-    users: database.users
-  })
+  res.status(200).json(usersView(database.users, 1, 1))
 })
 
 export default users
---
+++ src/views/users.ts
@@ -0,0 +1,34 @@
+import { User } from '@models/user'
+
+export interface UserView {
+  username: string,
+  email: string,
+  password?: string
+}
+
+export interface UsersView {
+  users: Array<UserView>,
+  page: number,
+  pages: number
+}
+
+export function usersView(users: Array<User>, page: number, pages: number): UsersView {
+  const view: UsersView = {
+    users: [],
+    page,
+    pages
+  }
+
+  users.forEach(user => {
+    view.users.push(userView(user))
+  })
+
+  return view
+}
+
+export function userView(user: User): UserView {
+  return {
+    username: user.username,
+    email: user.email
+  }
+}
