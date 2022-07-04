3ba8e60f0127df241911297c711433a289b33ee2
Thiago Rezende <thiago.manoel.rezende@gmail.com>
2022-07-04 15:10:22 +0000

    test: add test coverage to anime update route

--- tests/animes.spec.ts
+++ tests/animes.spec.ts
@@ -104,4 +104,97 @@ describe('Animes [v1]', () => {
     expect(res.statusCode).toBe(400)
     expect(res.body).toHaveProperty('error')
   })
+
+  test('[PATCH /v1/animes/:id] incorrect request body', async () => {
+    const user: User = users[0]
+    const anime: Anime = animes[0]
+
+    const auth = await request(server)
+      .post('/auth')
+      .send({ user: { email: user.email, password: user.password } })
+
+    const accessToken = auth.body.access_token
+
+    const res = await request(server)
+      .patch(`/v1/animes/${anime.id}`)
+      .set('Authorization', 'Bearer ' + accessToken)
+      .send({})
+
+    expect(res.statusCode).toBe(400)
+    expect(res.body).toHaveProperty('error')
+  })
+
+  test('[PATCH /v1/animes/:id] unique new fields', async () => {
+    const user: User = users[0]
+    const anime: Anime = animes[0]
+
+    const auth = await request(server)
+      .post('/auth')
+      .send({ user: { email: user.email, password: user.password } })
+
+    const accessToken = auth.body.access_token
+
+    let res = await request(server)
+      .patch(`/v1/animes/${anime.id}`)
+      .set('Authorization', 'Bearer ' + accessToken)
+      .send({
+        anime: {
+          name: 'Violet Evergarden Gaiden',
+          slug: 'violet-evergarden-gaiden',
+          native: 'ヴァイオレット・エヴァーガーデン 外伝',
+          romaji: 'Vaioretto Evāgāden Gaiden',
+          synopsis: 'Gaiden consists of six stories in total: the first one revolves around Charlotte\'s story. Another story will be centered on Benedict who visits a certain town with Violet due to work and they lose track of one another. Another will feature Cattleya, who gets proposed to by one of her clients, and while his personality has nothing to be criticized as a marriage partner, a “certain person” keeps surfacing in Cattleya’s mind. Another one will be about Gilbert and Hodgins, who met for the first time in the military academy that they attended. Hodgins reminisces about how he and Gilbert became friends and the things they had done. The last story features all the CH Postal Company characters. The company shows a rapid growth due to being supported by the public as they are even able to deliver letters to and from the battlefields.',
+          releaseDate: '2019-03-18T00:00:00.000Z'
+        }
+      })
+
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toHaveProperty('anime')
+
+    res = await request(server)
+      .patch(`/v1/animes/${anime.id}`)
+      .set('Authorization', 'Bearer ' + accessToken)
+      .send({
+        anime: {
+          name: anime.previous('name'),
+          slug: anime.previous('slug'),
+          native: anime.previous('native'),
+          romaji: anime.previous('romaji'),
+          synopsis: anime.previous('synopsis'),
+          releaseDate: anime.previous('releaseDate')
+        }
+      })
+
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toHaveProperty('anime')
+  })
+
+  test('[PATCH /v1/users/:id] non-unique new fields', async () => {
+    const user: User = users[0]
+    const anime: Anime = animes[0]
+    const otherAnime: Anime = animes[1]
+
+    const auth = await request(server)
+      .post('/auth')
+      .send({ user: { email: user.email, password: user.password } })
+
+    const accessToken = auth.body.access_token
+
+    const res = await request(server)
+      .patch(`/v1/animes/${anime.id}`)
+      .set('Authorization', 'Bearer ' + accessToken)
+      .send({
+        anime: {
+          name: otherAnime.name,
+          slug: otherAnime.slug,
+          native: otherAnime.native,
+          romaji: otherAnime.romaji,
+          synopsis: otherAnime.synopsis,
+          releaseDate: otherAnime.releaseDate
+        }
+      })
+
+    expect(res.statusCode).toBe(409)
+    expect(res.body).toHaveProperty('error')
+  })
 })
