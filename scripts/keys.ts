33ec82285a685cb29b29efa7145813028f355311
Thiago Rezende <thiago.manoel.rezende@gmail.com>
2022-07-09 11:48:31 -0300

    chore: move database scripts to scripts folder and add changed key pair generator to typescript

---
+++ scripts/keys.ts
@@ -0,0 +1,40 @@
+import { writeFileSync } from 'fs'
+import { generateKeyPair } from 'crypto'
+
+import config from '@config/jwt'
+
+type Command = 'generate'
+
+const args = process.argv.slice(2)
+
+const command: Command = args[0] as Command || 'generate'
+
+switch (command) {
+  case 'generate':
+    console.log('[keys] <generate> generating new key pair')
+
+    generateKeyPair('rsa', {
+      modulusLength: 4096,
+      publicKeyEncoding: {
+        type: 'spki',
+        format: 'pem'
+      },
+      privateKeyEncoding: {
+        type: 'pkcs8',
+        format: 'pem',
+        cipher: 'aes-256-cbc',
+        passphrase: config.passphrase
+      }
+    }, (_err, publicKey, privateKey) => {
+      writeFileSync('certs/jwtRS256.pem', publicKey)
+      console.log('[keys] <generate> <public> \'certs/jwtRS256.pem\'')
+
+      writeFileSync('certs/jwtRS256.key', privateKey)
+      console.log('[keys] <generate> <private> \'certs/jwtRS256.key\'')
+    })
+    break
+
+  default:
+    console.log('[keys] <error> invalid command')
+    break
+}
